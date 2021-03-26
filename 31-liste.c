3fd0efa6fa54606444c71e9d98ab92c5439ace43
glucatv <78474060+glucatv@users.noreply.github.com>
2021-03-26 16:52:46 +0100

    Add files via upload

--- 31-liste.c
+++ 31-liste.c
@@ -9,6 +9,19 @@ typedef struct nodo nodo;
 
 typedef nodo *lista;
 
+
+lista lista_vuota();
+lista lista_in0(lista, int);
+lista lista_in1(lista, int);
+lista lista_in(lista, int, int );
+void lista_print(lista);
+int lista_len(lista);
+nodo *lista_search(lista, int);
+
+/*
+ * Creazione di una nuova lista
+ * 
+ * */
 lista lista_vuota(){
 	return NULL;
 }
@@ -42,6 +55,20 @@ void lista_print(lista b){
 }
 
 /*
+ * Restituisce il numero di nodi della lista
+ * */
+int lista_len(lista b){
+	nodo *q = b;
+	int c = 0;
+	
+	while( q != NULL ){
+		c++;
+		q = q->succ;
+	}
+	return c;
+}
+
+/*
  * Restituisce l'indirizzo del nodo in posizione pos della lista b;
  * NULL se tale posizione non esiste.
  * */
@@ -61,10 +88,42 @@ nodo *lista_search(lista b, int pos){
 }
 
 /*
+ * Inserisce nuovo nodo in posizione 1
+ * */
+lista lista_in1(lista b, int k){
+	nodo *q;
+	if (b == NULL)
+		return b;
+		
+	q = malloc(sizeof(nodo));
+	q->chiave = k;
+	q->prec = b;
+	q->succ = b->succ;
+	if (b->succ != NULL)
+		b->succ->prec = q;
+	b->succ = q;
+	
+	return b;
+}
+
+
+/*
  * Inserimento in posizione pos
  * */
 lista lista_in(lista b, int pos, int k){
-	// TODO
+	nodo *p = lista_search(b, pos-1);
+	
+	if( pos == 0 ){
+		b = lista_in0(b, k);
+		return b;
+	}
+	
+	if (p == NULL)
+		return b;
+		
+	p = lista_in1(p, k);
+	
+	return b;
 }
 
 void main(){
@@ -75,6 +134,8 @@ void main(){
 	
 	for ( i = 0; i < 10; i++)
 		a = lista_in0(a, i);
+		
+	a = lista_in(a, lista_len(a), 100);
 		
 	lista_print( a );
 	
