5f2c566fb8bbcf49091b4f6bdb96077bfb01e0db
glucatv <78474060+glucatv@users.noreply.github.com>
2021-03-31 21:25:50 +0200

    Add files via upload
    Aggiunte funzioni di cancellazione

--- 31-liste.c
+++ 31-liste.c
@@ -14,6 +14,8 @@ lista lista_vuota();
 lista lista_in0(lista, int);
 lista lista_in1(lista, int);
 lista lista_in(lista, int, int );
+lista lista_out0(lista);
+lista lista_out(lista, int);
 void lista_print(lista);
 int lista_len(lista);
 nodo *lista_search(lista, int);
@@ -40,6 +42,105 @@ lista lista_in0(lista b, int k){
 	b = p;
 	return b;
 }
+
+/*
+ * Inserisce nuovo nodo in posizione 1
+ * */
+lista lista_in1(lista b, int k){
+	nodo *q;
+	if (b == NULL)
+		return b;
+		
+	q = malloc(sizeof(nodo));
+	q->chiave = k;
+	q->prec = b;
+	q->succ = b->succ;
+	if (b->succ != NULL)
+		b->succ->prec = q;
+	b->succ = q;
+	
+	return b;
+}
+
+/*
+ * Inserimento in posizione pos
+ * */
+lista lista_in(lista b, int pos, int k){
+	nodo *p = lista_search(b, pos-1);
+	
+	if( pos == 0 ){
+		b = lista_in0(b, k);
+		return b;
+	}
+	
+	if (p == NULL)
+		return b;
+		
+	p = lista_in1(p, k);
+	
+	return b;
+}
+
+
+/*
+ * Cancellazione primo nodo della lista
+ * */
+lista lista_out0(lista b){
+	nodo *t = b;
+	
+	if (b == NULL)
+		return b;
+		
+	b = b->succ;
+	
+	/*potrebbe succedere b == NULL*/
+	if ( b != NULL )
+		b->prec = NULL;
+	
+	free(t);
+	
+	return b;
+}
+
+
+/*
+ * Cancellazione del nodo in posizione 1
+ * */
+lista lista_out1(lista b){
+	nodo *p;
+	
+	if ( b == NULL || b->succ == NULL )
+		return b;
+	
+	p = b->succ;
+	b->succ = p->succ;
+	if ( p->succ != NULL )
+		p->succ->prec = b;
+	
+	free(p);
+	
+	return b;
+}
+
+/*
+ * Cancellazione di un nodo in posizione pos della lista
+ * */
+lista lista_out(lista b, int pos){
+	nodo *p;
+	
+	if (pos < 0)
+		return b;
+	
+	if (pos == 0){
+		b = lista_out0(b);
+		return b;
+	}
+	
+	p = lista_search(b, pos-1);
+	p = lista_out1(p);
+	
+	return b;
+} 
 
 /*
  * Stampa degli elementi della lista
@@ -87,44 +188,6 @@ nodo *lista_search(lista b, int pos){
 	return q;
 }
 
-/*
- * Inserisce nuovo nodo in posizione 1
- * */
-lista lista_in1(lista b, int k){
-	nodo *q;
-	if (b == NULL)
-		return b;
-		
-	q = malloc(sizeof(nodo));
-	q->chiave = k;
-	q->prec = b;
-	q->succ = b->succ;
-	if (b->succ != NULL)
-		b->succ->prec = q;
-	b->succ = q;
-	
-	return b;
-}
-
-
-/*
- * Inserimento in posizione pos
- * */
-lista lista_in(lista b, int pos, int k){
-	nodo *p = lista_search(b, pos-1);
-	
-	if( pos == 0 ){
-		b = lista_in0(b, k);
-		return b;
-	}
-	
-	if (p == NULL)
-		return b;
-		
-	p = lista_in1(p, k);
-	
-	return b;
-}
 
 void main(){
 	lista a = lista_vuota();
@@ -139,8 +202,12 @@ void main(){
 		
 	lista_print( a );
 	
-	p = lista_search(a, 15);
+	a = lista_out(a, 11);
 	
-	if (p != NULL)
-		printf("%d\n", p->chiave);
+	lista_print( a );
+	
+	while( a->succ != NULL ){
+		a = lista_out1(a);
+		lista_print(a);
+	}
 }
